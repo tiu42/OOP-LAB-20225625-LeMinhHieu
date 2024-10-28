3591ce489068beec2c545c7bdf00cbd843e11371
tiu42 <hieu04022004@gmail.com>
2024-10-28 20:50:25 +0700

    Updated Lab02

--- Lab02/AimsProject/src/my_pkg/Cart.java
+++ Lab02/AimsProject/src/my_pkg/Cart.java
@@ -7,7 +7,7 @@ public class Cart {
 	private int qtyOrdered = 0;
 	
 	public void addDVD(DigitalVideoDisc disc) {
-		if(this.qtyOrdered < 20) {
+		if(this.qtyOrdered < MAX_NUMBERS_ORDERED) {
 			orderedItems[qtyOrdered] = disc;
 			System.out.println("The disc has been added!");
 			qtyOrdered++;
@@ -18,11 +18,33 @@ public class Cart {
 	}
 	
 	public void removeDVD(DigitalVideoDisc disc) {
-		int revoved = 0;
 		for(int i=0; i<this.qtyOrdered;i++) {
-			if(orderedItems[i] == disc)
+			if(orderedItems[i] == disc) {
+				for (int j = i; j < qtyOrdered - 1; j++) {
+					orderedItems[j] = orderedItems[j + 1];
+                }
+				orderedItems[qtyOrdered - 1] = null;
+                qtyOrdered--;
+                System.out.println("The disc has been removed.");
+                return;
+			}
 		}
+		System.out.println("The disc is not in the cart.");
 	}
 	
+	public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+            total += orderedItems[i].getCost();
+        }
+        return total;
+    }
+
+    public void displayCart() {
+        for (int i = 0; i < qtyOrdered; i++) {
+            System.out.println(orderedItems[i].getTitle());
+        }
+        System.out.println("Total cost: " + totalCost());
+    }
 	
 }
